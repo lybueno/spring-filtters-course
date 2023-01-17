9e770c7dccef2835bbb5085dc2a5df8be6aa87d4
Lygia Bueno <lybueno@gmail.com>
2023-01-17 12:46:17 -0300

    Sort filtter added

--- src/main/java/io/github/lybueno/filtrosspring/constant/ApiConstants.java
+++ src/main/java/io/github/lybueno/filtrosspring/constant/ApiConstants.java
@@ -6,4 +6,6 @@ public class ApiConstants {
     public static final String PAGE_KEY = "page";
     public static final Integer DEFAULT_LIMIT = 10;
     public static final Integer DEFAULT_PAGE = 0;
+    public static final String SORT_KEY = "sort";
+    public static final String DEFAULT_SORT = "";
 }
--- src/main/java/io/github/lybueno/filtrosspring/model/FilterModel.java
+++ src/main/java/io/github/lybueno/filtrosspring/model/FilterModel.java
@@ -3,7 +3,10 @@ package io.github.lybueno.filtrosspring.model;
 import lombok.Data;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static io.github.lybueno.filtrosspring.constant.ApiConstants.*;
@@ -13,13 +16,35 @@ public class FilterModel {
 
     private Integer limit;
     private Integer page;
+    private String sort;
 
     public FilterModel(Map<String, String> params){
-        this.limit = params.containsKey(LIMIT_KEY) ? Integer.valueOf(params.get("limit")) : DEFAULT_LIMIT;
-        this.page = params.containsKey(PAGE_KEY) ? Integer.valueOf(params.get("page")) : DEFAULT_PAGE;
+        this.limit = params.containsKey(LIMIT_KEY) ? Integer.valueOf(params.get(LIMIT_KEY)) : DEFAULT_LIMIT;
+        this.page = params.containsKey(PAGE_KEY) ? Integer.valueOf(params.get(PAGE_KEY)) : DEFAULT_PAGE;
+        this.sort = params.containsKey(SORT_KEY) ? params.get(SORT_KEY) : DEFAULT_SORT;
     }
 
     public Pageable toSpringPegeable(){
-        return PageRequest.of(page, limit);
+        List<Sort.Order> orders = getOrders();
+        return PageRequest.of(page, limit, Sort.by(orders));
+    }
+
+    private List<Sort.Order> getOrders(){
+
+        List<Sort.Order> orders = new ArrayList<>();
+        String[] properties = sort.split(",");
+        for(String property : properties){
+            if(!property.trim().isEmpty()){
+                String column = "";
+                if(property.startsWith("-")){
+                    column = property.replace("-", "").trim();
+                    orders.add(Sort.Order.desc(column));
+                } else {
+                    column = property.trim();
+                    orders.add(Sort.Order.asc(column));
+                }
+            }
+        }
+        return orders;
     }
 }
